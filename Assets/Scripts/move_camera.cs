11f35152fd384b7dafc9d30805c07d36deddac3c
Fernando López Medina <flopmed@upv.edu.es>
2022-10-24 18:23:24 +0200

    prueba de commit

--- Assets/Scripts/move_camera.cs
+++ Assets/Scripts/move_camera.cs
@@ -7,6 +7,7 @@ public class move_camera : MonoBehaviour
     // Start is called before the first frame update
     public Transform cameraPosition;
 
+
     // Update is called once per frame
     void Update()
     {
