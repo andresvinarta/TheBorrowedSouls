0f6481f7e7f71f1378e0a1305ceb5c2a76ee2d84
Fernando López Medina <flopmed@upv.edu.es>
2022-12-01 14:23:55 +0100

    Menú

--- Assets/Scripts/menu_manager.cs
+++ Assets/Scripts/menu_manager.cs
@@ -5,9 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class menu_manager : MonoBehaviour
 {
+    public GameObject CanvasCreditos;
+    public GameObject CanvasPrincipal;
+
+    public void Update()
+    {
+
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            CanvasCreditos.SetActive(false);
+            CanvasPrincipal.SetActive(true);
+        }
+    }
     public void StartGame()
     {
         SceneManager.LoadScene("Nivel 2");
+    }
+
+    public void Creditos()
+    {
+        CanvasPrincipal.SetActive(false);
+        CanvasCreditos.SetActive(true);
     }
 
     public void ExitGame()
