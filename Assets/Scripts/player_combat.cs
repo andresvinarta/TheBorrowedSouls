c66b5f91d459cb3735a9e94a02582f5217354f49
Fernando López Medina <flopmed@upv.edu.es>
2022-12-01 12:08:19 +0100

    g1580 solo animaciones

--- Assets/Scripts/player_combat.cs
+++ Assets/Scripts/player_combat.cs
@@ -7,16 +7,33 @@ public class player_combat : MonoBehaviour
 
     public float playerHealth;
 
-    public GameObject WeaponHolder;
+    public GameObject arma;
     public GameObject guadana;
+    public Camera camara;
+    public Transform attackPoint;
+    public RaycastHit rayHit;
+    public LayerMask isEnemy;
     Animator guadanaAnim;
+    Animator pistolaAnim;
 
-    //Disparos
+    //Disparos stats
+    public int damage;
+    public float timeBetweenShooting, spread, range, reloadTime;
+    public int magazineSize;
+    int bulletsLeft;
+
+    //estados
+    bool shooting, readyToShoot, reloading;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        readyToShoot = true;
+        bulletsLeft = magazineSize;
+
+        arma.SetActive(true);
+        pistolaAnim = arma.GetComponent<Animator>();
         //guadana = WeaponHolder.gameObject.transform.GetChild(0).gameObject;
         guadana.SetActive(false);
         guadanaAnim = guadana.GetComponent<Animator>();
@@ -30,18 +47,45 @@ public class player_combat : MonoBehaviour
             Debug.Log("Tas muelto pibe");
         }
 
+        if (readyToShoot && Input.GetMouseButtonDown(0) && !reloading && bulletsLeft > 0)
+        {
+            Debug.Log("hola");
+            Shoot();
+        }
+
         if (Input.GetMouseButtonDown(1))
         {
-            WeaponHolder.SetActive(false);
+            arma.SetActive(false);
             guadana.SetActive(true);
             guadanaAnim.Play("MeleeGuadana");
             Invoke(nameof(MeleeReset), 0.7f);
-        } 
+        }
+
+        if (Input.GetKeyDown(KeyCode.R) && !reloading && bulletsLeft < magazineSize) Reload();
+    }
+
+    private void Shoot()
+    {
+        readyToShoot = false;
+
+        pistolaAnim.Play("Fuego1_Shot");
+
+        Invoke(nameof(ShootReset), timeBetweenShooting);
+    }
+
+    private void Reload()
+    {
+
+    }
+
+    private void ShootReset()
+    {
+        readyToShoot = true;
     }
 
     private void MeleeReset()
     {
-        WeaponHolder.SetActive(true);
+        arma.SetActive(true);
         guadana.SetActive(false);
     }
 
